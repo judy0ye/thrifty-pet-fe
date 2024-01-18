f2c884c53aa34503e22727369936f9060ecfb007
Judy Ye <judyy.ye@gmail.com>
2024-01-18 01:09:06 -0800

    Add Button theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import type { AppProps } from 'next/app';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { Button, createTheme, MantineProvider } from '@mantine/core';
 import HomeLayout from '@/components/templates/HomeLayout';
 import { useRouter } from 'next/router';
 import ProductLayout from '@/components/templates/ProductLayout';
 import { ReactNode } from 'react';
 
 const theme = createTheme({
-  /** Put your mantine theme override here */
+  components: {
+    Button: Button.extend({
+      defaultProps: {
+        color: "rgba(33, 30, 30, 0.96)",
+      },
+    }),
+  },
 });
 
 const LayoutSwitcher = ({children}: {children: ReactNode}) => {
