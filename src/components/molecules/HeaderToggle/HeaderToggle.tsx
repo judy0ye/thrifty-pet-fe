77e4ebc749676c4e98df35891e73a4adbdcb1e7d
Judy Ye <judyy.ye@gmail.com>
2024-01-21 13:45:35 -0800

    Add Mantine Anchor to link title to homepage

--- src/components/molecules/HeaderToggle/HeaderToggle.tsx
+++ src/components/molecules/HeaderToggle/HeaderToggle.tsx
@@ -1,13 +1,16 @@
-import { Burger, Group, Title } from "@mantine/core"
+import { Anchor, Burger, Group, Title } from "@mantine/core"
 import { type HeaderToggleTypes } from "./HeaderToggle.types";
+import Link from "next/link";
 
 export const HeaderToggle = ({mobileOpened, desktopOpened, toggleMobile, toggleDesktop}: HeaderToggleTypes) => {
 
   return (
-      <Group h={'100%'} px={'lg'}>
-        <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-        <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-        <Title order={1}>Thrifty Pet</Title>
-      </Group>
+    <Group h={'100%'} px={{base: 8, xs: 'xl', sm: 'md' }}>
+      <Burger aria-label="Toggle notes" opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+      <Burger aria-label="Toggle notes" opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+      <Title order={1}>
+        <Anchor fz={{base: 38, xs: 40, }} fw={'bold'} component={Link} href={'/'} underline="never" c={'black'}>Thrifty Pet</Anchor>
+      </Title>
+    </Group>
   )
 }
